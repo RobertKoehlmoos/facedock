57431d7bb5e1a3761850b1420f052991a0cfb98f
rob <robert.koehlmoos@gmail.com>
2022-06-27 18:27:11 -0400

    Minor async changes to improve performance

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import File, FastAPI, UploadFile
 from fastapi.responses import RedirectResponse
 from .photo_processing import get_embeddings
+import aiofiles
 import os
 
 app = FastAPI()
@@ -13,8 +14,9 @@ def read_root():
 
 @app.post("/photo")
 async def photo_embeddings(photo: UploadFile = File(...)):  # the = File(...) needs to be there. I can't figure out why.
+    # todo: choose locations that won't cause problems when multiple people make the same api call at once
     temp_photo_location = f"./{photo.filename}"
-    with open(temp_photo_location, "wb+") as temp_photo:
+    async with aiofiles.open(temp_photo_location, "wb+") as temp_photo:
         temp_photo.write(photo.file.read())
     result = get_embeddings(temp_photo_location)
     os.remove(temp_photo_location)
