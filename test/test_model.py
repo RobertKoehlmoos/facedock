f1c5daac9a7404ecdf7f21afe09ccf8cdf7debfa
rob <robert.koehlmoos@gmail.com>
2022-08-03 14:40:55 -0400

    Succesfully produced performance graphs

--- test/test_model.py
+++ test/test_model.py
@@ -4,6 +4,9 @@ import multiprocessing
 from deepface import DeepFace
 from functools import partial
 import json
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 
 def results_to_performance_comparable_dataframe(performance: list[dict]) -> pd.DataFrame:
@@ -11,6 +14,7 @@ def results_to_performance_comparable_dataframe(performance: list[dict]) -> pd.D
     filtered = [[result['age'], result['gender'], result['dominant_race']] for result in performance]
 
     # adjusting the values to match the classifications used in the provided correct answers
+    # change to use python match/case syntax during refactor
     for i, entry in enumerate(filtered):
         if entry[0] <= 2:
             entry[0] = "0-2"
@@ -28,7 +32,7 @@ def results_to_performance_comparable_dataframe(performance: list[dict]) -> pd.D
             entry[0] = "50-59"
         elif entry[0] < 70:
             entry[0] = "60-69"
-        elif entry[0] < 20:
+        else:
             entry[0] = "more than 70"
 
         if entry[1] == "Man":
@@ -49,11 +53,25 @@ def results_to_performance_comparable_dataframe(performance: list[dict]) -> pd.D
     return pd.DataFrame(columns=['file', 'age', 'gender', 'race'], data=filtered)
 
 
+def compare_performance(pred: pd.DataFrame, true: pd.DataFrame):
+    def generate_graph(attribute: str):
+        attribute_confusion_matrix = confusion_matrix(true.loc[:499, attribute], pred[attribute])
+        labels = true[attribute].unique()
+        labels.sort()
+        disp = ConfusionMatrixDisplay(confusion_matrix=attribute_confusion_matrix, display_labels=labels)
+        disp.plot()
+        plt.xticks(rotation=45)
+        plt.title(f"{attribute} Confusion Matrix")
+        plt.savefig(f"./figures/{attribute}_confusion_matrix.png", bbox_inches="tight")
+
+    generate_graph("gender")
+    generate_graph("race")
+    generate_graph("age")
 
 
 def evaluate_model_performance():
     # Runs the DeepFace model against the provided faces and then evaluates their performance
-    val = pd.read_csv("../../face_tests/fairface_label_train.csv")
+    true_classifications = pd.read_csv("../../face_tests/fairface_label_train.csv")
 
     default_analyze_kwargs = {"detector_backend": 'skip', "actions": ('age', 'gender', 'race'), "prog_bar": False,
                               "enforce_detection": False}
@@ -61,16 +79,22 @@ def evaluate_model_performance():
     directory = '../../face_tests/train_small_500'
     for filename in os.listdir(directory):
         photo_paths.append(os.path.join(directory, filename))
+    # the paths are sorted alphabetically, we really want them numerically
+    photo_paths_sorted = sorted(photo_paths, key=lambda x: int(x.split('\\')[-1][:-4]))
 
     with multiprocessing.Pool(processes=5) as pool:
-        results = pool.map(partial(DeepFace.analyze, **default_analyze_kwargs), photo_paths)
+        results = pool.map(partial(DeepFace.analyze, **default_analyze_kwargs), photo_paths_sorted)
     with open("train_small_results.json", "w") as f:
         json.dump(results, f)
 
-    results_to_performance_comparable_dataframe(results)
+    pred_classifications = results_to_performance_comparable_dataframe(results)
+    pred_classifications.to_csv("./deepface_classifications.csv")
+    compare_performance(pred_classifications, true_classifications)
 
 
 if __name__ == "__main__":
-    with open("train_small_results.json", "r") as f:
-        results = results_to_performance_comparable_dataframe(json.load(f))
-    results.to_csv("./deepface_classifications.csv")
+    # for a full run, use evaluate_model_performance. below is shortened for when you already have done the processing
+    # would probably work better as a jupyter notebook
+    true_classifications = pd.read_csv("../../face_tests/fairface_label_train.csv")
+    pred_classifications = pd.read_csv("./deepface_classifications.csv")
+    compare_performance(pred_classifications, true_classifications)
